d41cfa09be624fbedb4bb0e790e8ce97610130f6
Toshiki Fujiyoshi <toshiki.fujiyoshi@enechange.co.jp>
2018-07-20 17:21:38 +0900

    最初のコンポーネント

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # 以下を書くだけでよい
-  prepend_view_path Rails.root.join("frontend")
+  prepend_view_path Rails.root.join("app/frontend")
 end
